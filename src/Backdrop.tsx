2c96a83721a06cfc38e00e969ee10f9cb7ee52e2
Roxanne <1391753481@qq.com>
2021-10-13 23:11:52 +0800

    complete

---
+++ src/Backdrop.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+const Backdrop: React.FC<any> = ({ activeColor, trackIndex, isPlaying }) => {
+  useEffect(() => {
+    document.documentElement.style.setProperty('--active-color', activeColor);
+  }, [trackIndex, activeColor]);
+  return <div className={`color-backdrop ${isPlaying ? 'playing' : 'idle'}`} />;
+};
+
+export default Backdrop;
---
+++ src/tracks.ts
@@ -0,0 +1,28 @@
+import imgSrc from "./assets/demo1.jpeg";
+import imgSrc2 from "./assets/demo2.jpeg";
+import imgSrc3 from "./assets/demo3.jpeg";
+import cali from "./assets/Rolling Star.mp3";
+import fifty from "./assets/Taylor Swift - Blank Space.mp3";
+import iwonder from "./assets/Taylor Swift,Ed Sheeran - Everything Has Changed.mp3";
+export default [
+  {
+    title:'Cali',
+    image: imgSrc,
+  artist:'Wataboi',
+  audioSrc:cali,
+  color:'#00aeb0'
+},
+{
+    title: "50",
+    artist: "tobylane",
+    audioSrc: fifty,
+    image: imgSrc2,
+    color: "#ffb77a"
+  },
+  {
+    title: "I Wonder",
+    artist: "DreamHeaven",
+    audioSrc: iwonder,
+    image: imgSrc3,
+    color: "#5f9fff"
+  }]
